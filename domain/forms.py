8c35d4a44c6fecf97d8464b709138982f21d0299
adamflentz <aflentz@email.wm.edu>
2018-02-22 20:37:13 -0500

    added some css back

--- domain/forms.py
+++ domain/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class DomainForm(forms.Form):
-    domain_list = [('1', '.autos'), ('2', '.boats'), ('3', '.homes'), ('4', '.yachts'), ('5', '.motorcycles')]
-    domain = forms.CharField(label='Search Term', max_length=1000)
-    extension = forms.ChoiceField(label='Domain', choices=domain_list)
+    extension_list = [('.autos', '.autos'), ('.boats', '.boats'), ('.homes', '.homes'), ('.yachts', '.yachts'), ('.motorcycles', '.motorcycles')]
+    domain = forms.CharField(label='Search Term', max_length=50, widget=forms.TextInput(attrs={'class': 'form-control', 'size':50}))
+    extension = forms.ChoiceField(label='Domain', choices=extension_list, widget=forms.Select(attrs={'class': 'btn btn-primary dropdown-toggle'}))
